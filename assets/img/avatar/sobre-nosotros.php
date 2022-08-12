67af64fbf04abcd441afa2688ab42edeef8b0d01
KROHOFF <camilolehueigor@gmail.com>
2022-08-12 11:51:20 -0400

    first commit

---
+++ assets/img/avatar/sobre-nosotros.php
@@ -0,0 +1,128 @@
+
+        <section class="p-b-10">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8">
+                        <div class="heading-text heading-section">
+                            <h2>DevAustral</h2>
+                            <span class="lead">Somos una empresa de desarrollo de software web, conformada por un equipo de trabajo de ingenieros informáticos, emprendedores, profesionales y comprometidos con tu negocio. Nos caracterizamos por ser empáticos, innovadores, tener constante comunicación con el cliente y dar una solución eficaz a la necesidad de tu empresa.</span>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <img src="images/logos/logo-about.png"  width="100%" height="100%">
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div class="heading-text heading-section">
+                            <h2>Misión</h2>
+                                <span class="lead">Nuestra misión principal como empresa es desarrollar y diseñar páginas webs en un entorno amigable para el usuario, auto funcional y multiplataforma. De una manera profesional, utilizando las tecnologías webs actuales.
+                                Con el fin de generar un impacto digital en tu emprendimiento o empresa.</span>
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="heading-text heading-section">
+                            <h2>Visión</h2>
+                                <span class="lead"> DevAustral, es una empresa de desarrollo web emprendedora que tiene como objetivo ser reconocida por la innovación, profesionalidad y simpleza, gracias a la calidad de nuestros servicios.</span>
+                        </div>
+                    </div>
+                </div>                    
+            </div>
+        </section>
+        <section class="background-grey">
+            <div class="container">
+                <div class="heading-text heading-section text-center">
+                    <h2>NUESTRO EQUIPO</h2>
+                    <span class="lead">Comprometidos con el avance tecnológico.</span>
+                </div>
+                <div class="row team-members team-members-shadow m-b-40">                   
+                    <div class="col-lg-4">
+                        <div class="team-member">
+                            <div class="team-image">
+                                <img src="images/team/logo-blanco-fondo.png">
+                            </div>
+                            <div class="team-desc">
+                                <h3>Fabian Mansilla</h3>
+                                <div class="align-center">
+                                    <a class="btn btn-xs btn-slide btn-light" href="#">
+                                        <i class="fab fa-facebook-f"></i>
+                                        <span>Facebook</span>
+                                    </a>
+                                    <a class="btn btn-xs btn-slide btn-light" href="#" data-width="100">
+                                        <i class="fab fa-twitter"></i>
+                                        <span>Twitter</span>
+                                    </a>
+                                    <a class="btn btn-xs btn-slide btn-light" href="#" data-width="118">
+                                        <i class="fab fa-instagram"></i>
+                                        <span>Instagram</span>
+                                    </a>
+                                    <a class="btn btn-xs btn-slide btn-light" href="mailto:#" data-width="80">
+                                        <i class="icon-mail"></i>
+                                        <span>Mail</span>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="team-member">
+                            <div class="team-image">
+                                <img src="images/team/logo-negro-fondo.png">
+                            </div>
+                            <div class="team-desc">
+                                <h3>Humberto Jara</h3>
+                                <div class="align-center">
+                                    <a class="btn btn-xs btn-slide btn-light" href="#">
+                                        <i class="fab fa-facebook-f"></i>
+                                        <span>Facebook</span>
+                                    </a>
+                                    <a class="btn btn-xs btn-slide btn-light" href="#" data-width="100">
+                                        <i class="fab fa-twitter"></i>
+                                        <span>Twitter</span>
+                                    </a>
+                                    <a class="btn btn-xs btn-slide btn-light" href="#" data-width="118">
+                                        <i class="fab fa-instagram"></i>
+                                        <span>Instagram</span>
+                                    </a>
+                                    <a class="btn btn-xs btn-slide btn-light" href="mailto:#" data-width="80">
+                                        <i class="icon-mail"></i>
+                                        <span>Mail</span>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="team-member">
+                            <div class="team-image">
+                                <img src="images/team/logo-blanco-fondo.png">
+                            </div>
+                            <div class="team-desc">
+                                <h3>Javier Sanhueza</h3>  
+                                <div class="align-center">
+                                    <a class="btn btn-xs btn-slide btn-light" href="#">
+                                        <i class="fab fa-facebook-f"></i>
+                                        <span>Facebook</span>
+                                    </a>
+                                    <a class="btn btn-xs btn-slide btn-light" href="#" data-width="100">
+                                        <i class="fab fa-twitter"></i>
+                                        <span>Twitter</span>
+                                    </a>
+                                    <a class="btn btn-xs btn-slide btn-light" href="#" data-width="118">
+                                        <i class="fab fa-instagram"></i>
+                                        <span>Instagram</span>
+                                    </a>
+                                    <a class="btn btn-xs btn-slide btn-light" href="mailto:#" data-width="80">
+                                        <i class="icon-mail"></i>
+                                        <span>Mail</span>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+        </section>
